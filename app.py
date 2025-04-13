6a9079c5f98f4bf9ac876a17332c3854e4d70254
IT2017198 <IT20171988@my.sliit.lk>
2025-04-13 14:02:56 +0530

    re commit port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import os
 
 app = Flask(__name__)
 
@@ -31,4 +32,5 @@ def extract_fbs():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
